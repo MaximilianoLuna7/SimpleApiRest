be850f066957644b09cf590cd8a975f5ae653503
Maximiliano Luna <maxiespn21@gmail.com>
2023-12-21 19:56:03 -0300

    test: add test for UserService functionality

--- src/test/java/com/apirest/UserServiceTest.java
+++ src/test/java/com/apirest/UserServiceTest.java
@@ -1,4 +1,36 @@
 package com.apirest;
 
+import com.apirest.models.UserEntity;
+import com.apirest.repositories.UserRepository;
+import com.apirest.servicies.UserService;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
 public class UserServiceTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    void addUserShouldAddANewUser() {
+        UserEntity userToAdd = UserEntity.builder()
+                .firstName("John")
+                .lastName("Doe")
+                .email("john.doe@example.com")
+                .build();
+
+        when(userRepository.save(userToAdd)).thenReturn(userToAdd);
+
+        UserEntity AddedUser = userService.addUser(userToAdd);
+
+        assertThat(AddedUser).isEqualTo(userToAdd);
+    }
 }
