b198b8f2a36d7397cb383f4315b978f6380bc28c
Maximiliano Luna <maxiespn21@gmail.com>
2023-12-21 19:57:16 -0300

    feat: add UserRepository interface

---
+++ src/main/java/com/apirest/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.apirest.repositories;
+
+import com.apirest.models.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+}
