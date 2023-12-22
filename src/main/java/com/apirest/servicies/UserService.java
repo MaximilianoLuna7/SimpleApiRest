c1360acb0a615cd55dffabe3c8fcb374e61eacea
Maximiliano Luna <maxiespn21@gmail.com>
2023-12-21 22:25:20 -0300

    feat: improve service by adding exception handling

--- src/main/java/com/apirest/servicies/UserService.java
+++ src/main/java/com/apirest/servicies/UserService.java
@@ -1,16 +1,28 @@
 package com.apirest.servicies;
 
+import com.apirest.exceptions.UserValidationException;
 import com.apirest.models.UserEntity;
 import com.apirest.repositories.UserRepository;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Transactional
     public UserEntity addUser(UserEntity userToAdd) {
-        return userRepository.save(userToAdd);
+        try {
+            return userRepository.save(userToAdd);
+        } catch (DataIntegrityViolationException e) {
+            throw new UserValidationException("Email is already in use.", e);
+        } catch (ConstraintViolationException e) {
+            throw new UserValidationException("Validation error when creating user", e);
+        }
+
     }
 }
