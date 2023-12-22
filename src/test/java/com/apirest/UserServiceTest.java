d967a4c1b9b1b37c0c9653a918bff25867f33e69
Maximiliano Luna <maxiespn21@gmail.com>
2023-12-21 21:06:46 -0300

    test: improve addUser test with comments and add save method verification

--- src/test/java/com/apirest/UserServiceTest.java
+++ src/test/java/com/apirest/UserServiceTest.java
@@ -9,7 +9,7 @@ import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class UserServiceTest {
@@ -21,16 +21,22 @@ public class UserServiceTest {
 
     @Test
     void addUserShouldAddANewUser() {
+        //Given a user to add with correct values in their fields
         UserEntity userToAdd = UserEntity.builder()
                 .firstName("John")
                 .lastName("Doe")
                 .email("john.doe@example.com")
                 .build();
 
+        // When the user is added, simulate the save operation in the repository
         when(userRepository.save(userToAdd)).thenReturn(userToAdd);
 
+        // Perform the addUser operation
         UserEntity AddedUser = userService.addUser(userToAdd);
 
+        // Then assert that the added user is equal to the user provider
         assertThat(AddedUser).isEqualTo(userToAdd);
+        // Verify that the save operation in the repository was called exactly once
+        verify(userRepository, times(1)).save(any(UserEntity.class));
     }
 }
