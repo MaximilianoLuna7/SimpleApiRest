9c982cf055871137b83855aa8cf31e8b56d8409e
Maximiliano Luna <maxiespn21@gmail.com>
2023-12-21 21:02:14 -0300

    fix: add missing @Autowired annotation in UserRepository injection

--- src/main/java/com/apirest/servicies/UserService.java
+++ src/main/java/com/apirest/servicies/UserService.java
@@ -2,10 +2,12 @@ package com.apirest.servicies;
 
 import com.apirest.models.UserEntity;
 import com.apirest.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
     private UserRepository userRepository;
 
     public UserEntity addUser(UserEntity userToAdd) {
