d424a06a854e20a4a082e60b200d1798b08840e8
Maximiliano Luna <maxiespn21@gmail.com>
2023-12-27 07:59:55 -0300

    feat: added getUserById method

--- src/main/java/com/apirest/servicies/UserService.java
+++ src/main/java/com/apirest/servicies/UserService.java
@@ -25,4 +25,9 @@ public class UserService {
         }
 
     }
+
+    public UserEntity getUserById(Long searchedUserId) {
+        return userRepository.findById(searchedUserId)
+                .orElse(null);
+    }
 }
