21662b94f4d9ccef5bebccfa85562580cad4e309
Maximiliano Luna <maxiespn21@gmail.com>
2023-12-30 14:16:14 -0300

    feat: add getUser method

--- src/main/java/com/apirest/controllers/UserController.java
+++ src/main/java/com/apirest/controllers/UserController.java
@@ -5,10 +5,7 @@ import com.apirest.servicies.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -20,5 +17,11 @@ public class UserController {
     public ResponseEntity<UserEntity> createUser(@RequestBody UserEntity userToCreate) {
         UserEntity createdUser = userService.addUser(userToCreate);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserEntity> getUserById(@PathVariable Long userId) {
+        UserEntity userToGet = userService.getUserById(userId);
+        return ResponseEntity.ok(userToGet);
     }
 }
